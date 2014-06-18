d08b66e68a2e4431172de4eed11de5fc959485ea
GringaoGit <magnoux.andy@gmail.com>
2014-06-18 12:25:57 +0200

    certificat auto signed, with creation of masterkey first, next csr, and to finish the cert

---
+++ certificat/scriptcreationcertificat.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Automation create of certificat
+
+domain = $1
+echo $1.csr
+# Creation of private key
+# openssl genrsa -out masterca.key 2048
+openssl genrsa -des3 -out masterca.key 2048
+
+# Creation of csr
+openssl req -new -key masterca.key -out $1.csr
+
+# Testing csr
+openssl req -noout -text -in $1.csr
+echo "CSR OK"
+
+# Self signed the csr to get the certificat
+openssl x509 -req -days 365 -in $1.csr -signkey masterca.key -out $1.crt
+echo "Certificat self signed"
