5041938ffbd5a58d6ec2d866bedd704cf2b96c35
GringaoGit <magnoux.andy@gmail.com>
2014-07-05 16:51:13 +0200

    new change

--- httpClientServerC/server/http_server.c
+++ httpClientServerC/server/http_server.c
@@ -12,11 +12,11 @@
 
 char response[] = "HTTP/1.1 200 OK\r\n"	
 	"Content-Type: text/html; charset=UTF-8\r\n\r\n"
-	"<doctype !html><html><head><title>Bye-bye baby bye-bye</title>"
+	"<doctype !html><html><head><title>Esgi Is Here</title>"
 	"<style>body { background-color: #111 }"
 	"h1 { font-size:4cm; text-align: center; color: black;"
 	" text-shadow: 0 0 2mm red}</style></head>"
-	"<body><h1>Goodbye, world!</h1></body></html>\r\n";
+	"<body><h1>Hello, world!</h1></body></html>\r\n";
 
 int main(int Count, char *Strings[])
 {   int sockfd;
