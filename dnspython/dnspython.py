993bf3cc414ad51630cfa467f3a6ac67f1b99d43
GringaoGit <magnoux.andy@gmail.com>
2014-07-22 02:46:58 +0200

    add any records

--- dnspython/dnspython.py
+++ dnspython/dnspython.py
@@ -49,3 +49,13 @@ except dns.resolver.NoAnswer:
     print "*** No TXT record for", host, "***"
 except dns.resolver.NXDOMAIN:
     print "*** The domain", host, "does not exist ***"
+
+# Any records
+name_server = '8.8.8.8' #Google's DNS server
+ADDITIONAL_RDCLASS = 65535
+request = dns.message.make_query(host, dns.rdatatype.ANY)
+request.flags |= dns.flags.AD
+request.find_rrset(request.additional, dns.name.root, ADDITIONAL_RDCLASS,
+                       dns.rdatatype.OPT, create=True, force_unique=True)       
+response = dns.query.udp(request, name_server)
+print response
